ae0510cd8f7b855de3cdf69b5a0bb5301c428d8f
dunhui.xiao <dunhui.xiao@xiaobao100.com>
2019-08-06 19:09:25 +0800

    debug

--- Share.Web.User.Repository/UserDBContext.cs
+++ Share.Web.User.Repository/UserDBContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Share.Web.User.Repository
 {
@@ -20,89 +22,89 @@ namespace Share.Web.User.Repository
         /// 用户表
         /// </summary>
         public DbSet<UserRepo> UserRepos { get; set; }
+    }
 
-        public static class Extend
+    public static class Extend
+    {
+        public static IList<T> SqlQuery<T>(this DatabaseFacade database, string sql, params object[] parameters)
+                 where T : new()
         {
-            public static IList<T> SqlQuery<T>(this DatabaseFacade database, string sql, params object[] parameters)
-                     where T : new()
+            //注意：不要对GetDbConnection获取到的conn进行using或者调用Dispose否则bContext后续不能再进行使用了，会抛异常
+            var conn = database.GetDbConnection();
+            try
             {
-                //注意：不要对GetDbConnection获取到的conn进行using或者调用Dispose否则bContext后续不能再进行使用了，会抛异常
-                var conn = database.GetDbConnection();
-                try
+                conn.Open();
+                using (var command = conn.CreateCommand())
                 {
-                    conn.Open();
-                    using (var command = conn.CreateCommand())
+                    command.CommandText = sql;
+                    command.Parameters.AddRange(parameters);
+
+                    var rtnList = new List<T>();
+
+                    //如果返回类型不是类 类型，则只进行首行首列的查询【string is class】
+                    if (!typeof(T).IsClass)
                     {
-                        command.CommandText = sql;
-                        command.Parameters.AddRange(parameters);
+                        var result = command.ExecuteScalar();
+                        rtnList.Add((T)result);
+                        return rtnList;
+                    }
 
-                        var rtnList = new List<T>();
-
-                        //如果返回类型不是类 类型，则只进行首行首列的查询【string is class】
-                        if (!typeof(T).IsClass)
+                    var propts = typeof(T).GetProperties();
+                    T model;
+                    object val;
+                    //查询结果的列中不存在模型字段
+                    List<string> nonExistentfields = new List<string>();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
                         {
-                            var result = command.ExecuteScalar();
-                            rtnList.Add((T)result);
-                            return rtnList;
-                        }
-
-                        var propts = typeof(T).GetProperties();
-                        T model;
-                        object val;
-                        //查询结果的列中不存在模型字段
-                        List<string> nonExistentfields = new List<string>();
-                        using (var reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
+                            model = new T();
+                            foreach (var p in propts)
                             {
-                                model = new T();
-                                foreach (var p in propts)
+                                if (nonExistentfields.Any(o => o == p.Name))
                                 {
-                                    if (nonExistentfields.Any(o => o == p.Name))
+                                    continue;
+                                }
+                                try
+                                {
+                                    val = reader[p.Name];
+                                    if (val == DBNull.Value)
                                     {
-                                        continue;
+                                        p.SetValue(model, null);
                                     }
-                                    try
+                                    else
                                     {
-                                        val = reader[p.Name];
-                                        if (val == DBNull.Value)
+                                        //处理bit 转 bool 的问题
+                                        if (reader.GetDataTypeName(reader.GetOrdinal(p.Name)).ToLower() == "bit")
                                         {
-                                            p.SetValue(model, null);
+                                            p.SetValue(model, val.GetHashCode() == 1);
                                         }
                                         else
                                         {
-                                            //处理bit 转 bool 的问题
-                                            if (reader.GetDataTypeName(reader.GetOrdinal(p.Name)).ToLower() == "bit")
-                                            {
-                                                p.SetValue(model, val.GetHashCode() == 1);
-                                            }
-                                            else
-                                            {
-                                                p.SetValue(model, val);
-                                            }
+                                            p.SetValue(model, val);
+                                        }
 
-                                        }
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        if (ex is IndexOutOfRangeException)
-                                        {
-                                            nonExistentfields.Add(p.Name);
-                                            continue;
-                                        }
-                                        throw ex;
                                     }
                                 }
-                                rtnList.Add(model);
+                                catch (Exception ex)
+                                {
+                                    if (ex is IndexOutOfRangeException)
+                                    {
+                                        nonExistentfields.Add(p.Name);
+                                        continue;
+                                    }
+                                    throw ex;
+                                }
                             }
+                            rtnList.Add(model);
                         }
-                        return rtnList;
                     }
+                    return rtnList;
                 }
-                finally
-                {
-                    conn.Close();
-                }
+            }
+            finally
+            {
+                conn.Close();
             }
         }
     }
